ea97a1732a65e1bc1950daeef41c5f399aa580a5
joe <joe.tessier1995@gmail.com>
2018-10-09 23:05:10 +0200

    menu + activities display

--- app/src/main/java/com/example/josephtessier/gphyexam/MainActivity.java
+++ app/src/main/java/com/example/josephtessier/gphyexam/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.josephtessier.gphyexam;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, R.string.welcome_message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_webIcon){
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.univ-poitiers.fr"))); // bitch i'm a one-liner !
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
